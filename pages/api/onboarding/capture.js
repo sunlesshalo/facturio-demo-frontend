1350818ebf2efb88ae5e190ae15fbd371ccccb1e
Ferencz Csuszner <39485506-ferencz1@users.noreply.replit.com>
2025-02-15 10:18:56 +0000

    changed fetch link for capture endpoint

--- pages/api/onboarding/capture.js
+++ pages/api/onboarding/capture.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const response = await fetch('https://facturio-demo-backend-ferencz1.replit.app/onboarding/capture', {
+      const response = await fetch('https://facturio-test-ea-011-alpha-ferencz1.replit.app//onboarding/capture', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req.body),
